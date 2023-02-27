6845fed3b71fb476b2a9b5454866cbb4bb483a84
Dylan-CS <1003347510@qq.com>
2023-02-27 20:59:17 +0800

    1111

--- ReadMe.md
+++ ReadMe.md
@@ -1,2 +1,2 @@
- PestoGreen deployed at: 0x1996Db7C33B49FeA28b0aed9A3A1A12e3e12ec5c  
- TokenFaucet deployed at: 0x0006B9BAf4315f1884cEd26925a3eC069159F3a4
+  PestoGreen deployed at: 0xe65689c12065E4E8D718558216A7bFA5b8614Be1    
+  TokenFaucet deployed at: 0x87127020C92d70A5E45AEa6112DabE4d6C0fF5c3
--- scripts/deploy.py
+++ scripts/deploy.py
@@ -5,10 +5,12 @@ from scripts.helpful_scripts import get_account
 def main():
     account = get_account()
     # 部署代币合约
-    token = PestoGreen.deploy("PestoGreen", "PG", {'from': account})
+    token = PestoGreen.deploy(
+        "PestoGreen", "PG", {'from': account}, publish_source=True)
 
     # 部署Token Faucet合约并传入代币地址
-    faucet = TokenFaucet.deploy(token.address, {'from': account})
+    faucet = TokenFaucet.deploy(
+        token.address, {'from': account}, publish_source=True)
 
     # 转移一些代币到Token Faucet合约地址
     token.transfer(faucet.address, 1000 * 10 **
